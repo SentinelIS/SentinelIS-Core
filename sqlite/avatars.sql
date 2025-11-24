7fc04651e65b959ab766ded7dd2b66b010fc0b64
Nathan Dion Schmid <nathandion.schmid@gmail.com>
2025-11-24 13:45:16 +0100

    added SQLite Service to docker-compose

---
+++ sqlite/avatars.sql
@@ -0,0 +1,8 @@
+/*
+===================================================
+This is the SQL File for the SQLite database table.
+
+    For SQLite Web GUI access, follow this link:
+        http://localhost:8081/
+===================================================
+*/
